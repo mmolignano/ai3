500efae3b347389335bcdc2f1511069eb0058d88
chris.pardy <chris.pardy@c842427a-1736-11df-914e-2594af177612>
2010-02-19 16:46:46 +0000

    Unigram and Bigram models

---
+++ Group15_AI3/src/AI3.java
@@ -0,0 +1,16 @@
+
+public class AI3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		if (args.length < 2){
+			System.out.println("Usage: [Model] Train Test");
+		}else{
+			
+		}
+	}
+
+}
